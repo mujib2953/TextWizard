c335b8874815527b6510a2425e452d4b95bcaf5f
mujib2953 <ansarimujiburrehman@gmail.com>
2024-06-22 20:20:08 -0400

    react-quill default editor added.

--- frontend/src/pages/QuillEditorPage.js
+++ frontend/src/pages/QuillEditorPage.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+
+import "react-quill/dist/quill.snow.css";
+
 const QuillEditorPage = () => {
+    const [contentHtml, setContentHtml] = useState("");
+
     return (
         <div className="quill-editor-page">
             This is Quill Editor Page.
+            <ReactQuill
+                theme="snow"
+                value={contentHtml}
+                onChange={setContentHtml}
+            />
         </div>
     );
 };
