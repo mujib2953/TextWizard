dae7acc6ccb0fc3f912abf1ed68c69e717e1d1c3
mujib2953 <ansarimujiburrehman@gmail.com>
2024-06-23 20:29:03 -0400

    file upload & download implemented backend and frontend.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const path = require("node:path");
 const fs = require("node:fs");
+const multer = require("multer");
 
 const app = express();
 
 // --- for json handling
 app.use(express.json());
 const filesDirectoryPath = path.join(__dirname, "files");
+const uploadDirPath = path.join(filesDirectoryPath, "uploads");
+
+// --- file upload on local file-system
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, uploadDirPath);
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
 
 app.get("/api/v1/files", (req, res) => {
     fs.readdir(filesDirectoryPath, (err, files) => {
@@ -14,7 +28,9 @@ app.get("/api/v1/files", (req, res) => {
             return res.status(500).send("Error reading files");
         }
 
-        res.send(files);
+        // --- filtering upload folder
+        // --- TODO: search for better approach
+        res.send(files.filter(f => f !== "uploads"));
     });
 });
 
@@ -47,6 +63,28 @@ app.put("/api/v1/files/:filename", (req, res) => {
    });
 });
 
+app.post("/api/v1/upload", upload.array("files"), (req, res) => {
+    res.send("Upload file successfully.");
+});
+
+app.get("/api/v1/upload", (req, res) => {
+   fs.readdir(uploadDirPath, (err, files) => {
+       if (err) {
+           console.log(err)
+           res.status(500).send("Error reading file.");
+       }
+
+       res.send(files);
+   });
+});
+
+app.get("/api/v1/download/:filename", (req, res) => {
+    const { params: { filename } } = req;
+    const filePath = path.join(uploadDirPath, filename);
+
+    res.download(filePath, filename);
+});
+
 app.listen(process.env.PORT || 9002, () => {
-    console.log("Server running on port " + process.env.PORT || 9002);
+    console.log("Server running on port " + (process.env.PORT || 9092));
 });
--- frontend/src/pages/FilesPage.js
+++ frontend/src/pages/FilesPage.js
@@ -1,7 +1,164 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {
+    FaFileExcel,
+    FaFileWord,
+    FaFilePowerpoint,
+    FaFileCsv,
+    FaFilePdf,
+    FaFileZipper,
+    FaFileCode,
+    FaFileVideo,
+    FaFileAudio,
+    FaFileImage,
+    FaFile
+} from "react-icons/fa6";
+
+const iconsDict = {
+    excel: ["xls", "xlsx", "xlsx"],
+    word: ["doc", "docx", "docm", "dotx"],
+    powerpoint: ["ppt", "pptx"],
+
+    csv: ["csv"],
+
+    pdf: ["pdf"],
+    zip: ["zip", "rar"],
+    code: [".html", ".css", ".js", ".java", ".jsx"],
+
+    video: ["mp4", "avi"],
+    audio: ["mp3"],
+    image: ["jpg", "jpeg", "png"],
+};
+
 const FilesPage = () => {
+
+    const [selectedFiles, setSelectedFiles] = useState([]);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("/api/v1/upload")
+            .then(res => setUploadedFiles(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    const handleFilesUpload = () => {
+        const formData = new FormData();
+
+        for (let i = 0; i < selectedFiles.length; i++) {
+            formData.append("files", selectedFiles[i]);
+        }
+
+        axios
+            .post(`/api/v1/upload`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            })
+            .then(res => console.log(res.data))
+            .catch(err => console.error(err));
+    };
+
+    const handleFileDownload = (filename) => {
+        axios({
+            url: `/api/v1/download/${filename}`,
+            method: "GET",
+            responseType: "blob",
+
+        })
+            .then(res => {
+                const url = window.URL.createObjectURL(new Blob([res.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', filename); // or any other extension
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(err => console.error(err));
+    };
+
+    const getFileIcon = (extension) => {
+        for (let key in iconsDict) {
+            if (iconsDict[key].includes(extension)) {
+                switch (key) {
+                    case "excel":
+                        return <FaFileExcel />;
+                    case "word":
+                        return <FaFileWord />;
+                    case "powerpoint":
+                        return <FaFilePowerpoint />;
+
+                    case "csv":
+                        return <FaFileCsv />;
+
+                    case "pdf":
+                        return <FaFilePdf />;
+                    case "zip":
+                        return <FaFileZipper />;
+                    case "code":
+                        return <FaFileCode />;
+                    case "video":
+                        return <FaFileVideo />;
+                    case "audio":
+                        return <FaFileAudio />;
+                    case "image":
+                        return <FaFileImage />;
+                }
+            }
+        }
+
+        return <FaFile />;
+    };
+
     return (
         <div className="files-page">
-            This is Files Page.
+            <div className="upload-container">
+                <div>
+                    <input
+                        type="file"
+                        multiple={true}
+                        onChange={({target: {files}}) => setSelectedFiles(files)}
+                    />
+
+                    <button
+                        onClick={handleFilesUpload}
+                        disabled={selectedFiles.length === 0}
+                    >Upload
+                    </button>
+                </div>
+            </div>
+
+            <div className="upload-files-container">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {uploadedFiles.map((file) => {
+                            const _split = file.split(".")
+                            const [ext, ...name] = _split.reverse();
+
+                            return (
+                                <tr
+                                    key={file}
+                                    onClick={() => handleFileDownload(file)}
+                                >
+                                    <td>{name}</td>
+                                    <td>
+                                        { getFileIcon(ext) }
+                                        {ext}
+                                    </td>
+                                </tr>
+                            );
+
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
